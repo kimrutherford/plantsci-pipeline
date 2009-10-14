f53ff06df870ffd4299e89beb3e0b3cc615a4bbe
kmr44 <kmr44@721a6f05-5756-4a90-b3fc-8e2b6c5fda0e>
2009-10-14 15:53:09 +0000

    Calculate stats for non_genome_aligned reads and tags.
    git-svn-id: file:///data/subversion/smallrna/pipeline/trunk@996 721a6f05-5756-4a90-b3fc-8e2b6c5fda0e

--- script/load_static.pl
+++ script/load_static.pl
@@ -758,6 +758,36 @@ my @analyses = (
                     ]
                 },
                 {
+                 type_term_name => 'calculate fasta or fastq file statistics',
+                 runable_name => 'SmallRNA::Runable::FastStatsRunable',
+                 inputs => [
+                     {
+                       format_type => 'fasta',
+                       content_type => 'non_genome_aligned_srna_reads',
+                     }
+                    ]
+                },
+                {
+                 type_term_name => 'calculate fasta or fastq file statistics',
+                 runable_name => 'SmallRNA::Runable::FastStatsRunable',
+                 inputs => [
+                     {
+                       format_type => 'fasta',
+                       content_type => 'non_genome_aligned_genomic_dna_tags',
+                     }
+                    ]
+                },
+                {
+                 type_term_name => 'calculate fasta or fastq file statistics',
+                 runable_name => 'SmallRNA::Runable::FastStatsRunable',
+                 inputs => [
+                     {
+                       format_type => 'fasta',
+                       content_type => 'non_genome_aligned_genomic_dna_reads'
+                     }
+                    ]
+                },
+                {
                  type_term_name => 'summarise fasta first base',
                  runable_name => 'SmallRNA::Runable::FirstBaseCompSummaryRunable',
                  inputs => [
