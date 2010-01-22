98da176c3e74617daf033e9d1aad29fcf9fbf831
kmr44 <kmr44@721a6f05-5756-4a90-b3fc-8e2b6c5fda0e>
2010-01-22 17:26:50 +0000

    Copy properties to the non-aligned reads file.
    git-svn-id: file:///data/subversion/smallrna/pipeline/trunk@1348 721a6f05-5756-4a90-b3fc-8e2b6c5fda0e

--- lib/SmallRNA/Runable/AlignmentRunable.pm
+++ lib/SmallRNA/Runable/AlignmentRunable.pm
@@ -185,7 +185,10 @@ EVAL_BLOCK
         $self->store_pipedata(generating_pipeprocess => $self->pipeprocess(),
                               file_name => $non_aligned_file_name,
                               format_type_name => 'fasta',
-                              content_type_name => 'non_aligned_reads');
+                              content_type_name => 'non_aligned_reads',
+                              properties => { 'alignment component' => $component,
+                                              'alignment ecotype' => $org_full_name,
+                                              'alignment program' => $alignment_program });
       }
     } else {
       croak ("can't understand detail: $1 for pipeprocess: ", $pipeprocess_id);
